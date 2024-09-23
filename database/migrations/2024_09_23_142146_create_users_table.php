9242b9143638c5a0da5e084f1ef02d48f27e388a
jirbthagoras <hansjabriel@gmail.com>
2024-09-23 21:25:43 +0700

    Start User Model making

--- database/migrations/2024_09_23_142146_create_users_table.php
+++ database/migrations/2024_09_23_142146_create_users_table.php
@@ -13,11 +13,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string("useraname", 100)->unique("users_username_unique")->nullable(false);
+            $table->string("password", 100)->nullable(false);
+            $table->string("name", 100)->nullable(false);
+            $table->string("token", 100)->nullable()->unique("users_token_unique");
             $table->timestamps();
         });
     }
