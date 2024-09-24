03d9636f4f5bd6ee3188a009e8423aa6e5876c0e
jirbthagoras <hansjabriel@gmail.com>
2024-09-24 08:25:26 +0700

    Making User Request and Resource

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Address extends Model
 {
-    use HasFactory;
+
+    protected $table = "addresses";
+    protected $primaryKey = "id";
+    protected $keyType = "integer";
+    public $incrementing = true;
+    public $timestamps = true;
+
+    public function contact(): BelongsTo
+    {
+        return $this->belongsTo(Contact::class, "contact_id", "id");
+    }
+
 }
